b385cab72a2ab14def5bf523e785b9c07b421009
wangqi <x913724685@live.com>
2019-06-02 22:29:47 +0800

    init yes

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # GamesAndUselessGadgets
 Some games and useless gadgets implemented in dotnet.
+
+## list (not complete)
+
+- **yes** <https://en.wikipedia.org/wiki/Yes_(Unix)>
+
+程序已经写好，但是说明还没想好怎么写。
+程序中也没有注释。
---
+++ yes/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace yes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string content = "yes";
+            if(args.Length>0)
+                content = args[0];
+            while(true)
+                Console.WriteLine(content);
+        }
+    }
+}
