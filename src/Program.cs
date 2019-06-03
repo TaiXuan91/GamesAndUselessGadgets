6a9e4c9e4f3bf7b613f8788a4297914ee7e99777
wangqi <x913724685@live.com>
2019-06-03 14:15:52 +0800

    change name

--- src/Program.cs
+++ src/Program.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 
-namespace yes
+namespace GamesAndUselessGadgets
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+        }
+
+        static void yesMain(string[] args)
+        {
+            /// <summary>反正这是一段注释和咯噢噢噢噢nhaoesuhaoeu
+            /// </summary>
             string content = "yes";
             if(args.Length>0)
                 content = args[0];
